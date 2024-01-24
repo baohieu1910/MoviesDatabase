699bb547b640bef19175af65d2b7a575810c6cd0
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2024-01-25 00:37:10 +0700

    Initial Commit

---
+++ MoviesDatabase/MoviesDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDatabaseApp.swift
+//  MoviesDatabase
+//
+//  Created by Hieu Le on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
